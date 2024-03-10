c6bd119fd9e43272fdd6e20cf250da5912c3fc2d
Karthik H Kamath <74257697+KarthikhKamath@users.noreply.github.com>
2024-03-10 14:16:53 +0530

    Update jobSchema.js

--- backend/models/jobSchema.js
+++ backend/models/jobSchema.js
@@ -4,14 +4,11 @@ const jobSchema = new mongoose.Schema({
   title: {
     type: String,
     required: [true, "Please provide a title."],
-    minLength: [3, "Title must contain at least 3 Characters!"],
     maxLength: [30, "Title cannot exceed 30 Characters!"],
   },
   description: {
     type: String,
     required: [true, "Please provide decription."],
-    minLength: [30, "Description must contain at least 30 Characters!"],
-    maxLength: [500, "Description cannot exceed 500 Characters!"],
   },
   category: {
     type: String,
@@ -28,21 +25,17 @@ const jobSchema = new mongoose.Schema({
   location: {
     type: String,
     required: [true, "Please provide location."],
-    minLength: [20, "Location must contian at least 20 characters!"],
   },
   fixedSalary: {
     type: Number,
-    minLength: [4, "Salary must contain at least 4 digits"],
     maxLength: [9, "Salary cannot exceed 9 digits"],
   },
   salaryFrom: {
     type: Number,
-    minLength: [4, "Salary must contain at least 4 digits"],
     maxLength: [9, "Salary cannot exceed 9 digits"],
   },
   salaryTo: {
     type: Number,
-    minLength: [4, "Salary must contain at least 4 digits"],
     maxLength: [9, "Salary cannot exceed 9 digits"],
   },
   expired: {
