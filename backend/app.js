76777eba7eb65ebfb73a21f7a07b637c284e3729
Karthik H Kamath <74257697+KarthikhKamath@users.noreply.github.com>
2024-03-10 12:45:13 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://kartjobs.netlify.app",
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true,
 };
