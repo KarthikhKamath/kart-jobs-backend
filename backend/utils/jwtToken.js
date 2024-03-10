feae6181be729f81fe2fed414e302933b96f719c
Karthik H Kamath <74257697+KarthikhKamath@users.noreply.github.com>
2024-03-10 12:21:57 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,7 +5,7 @@ export const sendToken = (user, statusCode, res, message) => {
     Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
   ),
   httpOnly: true,
-  domain:"kartjobs.netlify.app"
+  domain:"https://kartjobs.netlify.app"
 };
 
 
